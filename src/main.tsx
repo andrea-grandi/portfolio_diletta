dafdb42d9b2096a073c23457fdb5341e5ca1fbb7
Andrea Grandi <andrea.grandi@tiscali.it>
2026-04-21 10:28:41 +0200

    fix: 404 redirect

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './styles/global.css'
 import App from './App.tsx'
 
+const redirect = sessionStorage.getItem("redirect");
+sessionStorage.removeItem("redirect");
+
+if (redirect && redirect !== location.pathname) {
+  history.replaceState(null, "", redirect);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
